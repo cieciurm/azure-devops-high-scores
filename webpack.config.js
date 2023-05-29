40be54dfc86e76ba0c316ce47e86f1f4cd6ca1ec
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2023-05-29 13:58:03 +0200

    Move JS to separate file and use webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+    entry: "./src/index.js",
+    mode: "production",
+    devtool: "source-map",
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+    },
     plugins: [
         new CopyPlugin({
             patterns: [
